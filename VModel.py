51de56a17a61d63a520d36e51d1d7fc5a9af5f22
TFujishima <None>
2018-03-31 18:52:07 +0900

    Instant-ModelImageCreater,Viewer

---
+++ VModel.py
@@ -0,0 +1,39 @@
+# instant-model viewer by TFujishima
+
+import keras
+from keras.models import Model
+from keras.utils.vis_utils import model_to_dot
+from PIL import Image
+import matplotlib.pyplot as plt
+from io import BytesIO
+
+from keras.layers import Dense
+from keras.layers import Input
+from keras.layers.merge import add
+
+
+# Viewing function attached with Model class
+class VModel(Model):
+	def __init__(self,inputs,outputs):
+		super().__init__(inputs,outputs)
+		self.pnb = model_to_dot(self).create_png()
+		self.png = Image.open(BytesIO(self.pnb))
+	def show(self):
+		plt.imshow(self.png)
+		plt.show()
+	def save(self,FilePath):
+		png.save(FilePath)
+		print('Model-image is saved as ' + FilePath + '.')
+
+
+
+
+def TestRun():
+	inp = Input((2,))
+	den = Dense(3,activation='relu')(inp)
+	den = Dense(2,activation='sigmoid')(den)
+	s = VModel(inp,den)
+	s.show()
+
+if __name__ == '__main__':
+	TestRun()
